83cef073beb5e190d604809d9e888766b83f06cb
cvaize <cvaize@gmail.com>
2022-04-28 17:24:30 +0500

    working

---
+++ clear-dataset-analysis.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const csv = require('csv-parser')
+const HTMLParser = require('node-html-parser');
+const rimraf = require("rimraf");
+
+
+const clean = function (rowObject) {
+
+    let link = rowObject.link;
+    if (link && link.includes('http')) {
+        let splitedUrl = link.split('/');
+        let repFolder = splitedUrl[3];
+        let repName = splitedUrl[4];
+        if (repFolder && repName) {
+            let filepath = `./dataset/analysis/${repFolder}/${repName}/phpmetrics.html`;
+
+            if (fs.existsSync(filepath)) {
+                let phpmetrics = fs.readFileSync(filepath, 'utf-8');
+
+                let root = HTMLParser.parse(phpmetrics);
+                let rows = root.querySelectorAll('#score table tbody tr');
+
+                if (!rows || !rows.length) {
+                    rimraf.sync(`./dataset/analysis/${repFolder}/${repName}`);
+                    console.log(`Removed - ./dataset/analysis/${repFolder}/${repName}`)
+                }
+            }
+
+        }
+    }
+
+    return rowObject;
+}
+
+let csvRows = [];
+
+fs.createReadStream('./dataset/60k_php_dataset_for_labelling.csv')
+    .pipe(csv())
+    .on('data', (data) => csvRows.push(data))
+    .on('end', () => {
+        csvRows.map(clean)
+        // [
+        //   { NAME: 'Daffy Duck', AGE: '24' },
+        //   { NAME: 'Bugs Bunny', AGE: '22' }
+        // ]
+    });
--- run_with_env_dataset.sh
+++ run_with_env_dataset.sh
@@ -20,7 +20,7 @@ do
         find ./code -iname "*test*.*" -exec rm -rf {} \;
         docker run --user $(id -u):$(id -g) --rm -v "$PWD/code":/app -v  "$PWD/dataset/analysis/${file_path}":/analysis \
         zdenekdrahos/phpqa:v1.25.0-php7.2 phpqa --tools phpmetrics,phpmd,pdepend,phpcs,phpcpd,phploc \
-        --ignoredDirs build,vendor,tests,lib,uploads,phpMyAdmin,phpmyadmin \
+        --ignoredDirs build,vendor,tests,lib,uploads,phpMyAdmin,phpmyadmin,library \
         --analyzedDirs /app --buildDir /analysis
     fi
     
